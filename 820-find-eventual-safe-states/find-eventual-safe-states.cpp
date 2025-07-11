6b4dbb1392cbbd1782bd4365d55e55c8df99d7a4
Yagyeshz Vishnoi <154703590+yagyeshvishnoi@users.noreply.github.com>
2025-07-11 19:06:48 +0530

    Time: 15 ms (66.03%) | Memory: 51.9 MB (88.66%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,37 +1,55 @@
-class Solution{
+class Solution {
 public:
-
-    bool dfs(vector<vector<int>> &graph, vector<int> &terminal, vector<int> &vis,int curr){
-        if(vis[curr] == 1)return false;
-        if(terminal[curr] == 1)return true;
-        if(terminal[curr] == -1)return false;
-
-        vis[curr] = true;
-        for(int i = 0 ; i<graph[curr].size();i++){
-            int num = graph[curr][i];
-            if(!dfs(graph,terminal,vis,num)){
-
-                terminal[curr] = -1;
-                return false;
-            }
+    bool helper(vector<vector<int>>& graph, vector<int>& issafe, vector<bool>& visit, int curr){
+        if(graph[curr].size() == 0 || issafe[curr] == 1){
+            issafe[curr] = 1;
+            return true;
         }
-        terminal[curr] = 1;
-        vis[curr] = 0;
+        
+        for(auto c : graph[curr]){
+            if(visit[c]) return false;
+            if(issafe[c] == 0){
+                visit[c] = true;
+                if(helper(graph, issafe, visit, c)){
+                    issafe[c] = 1;
+                }
+                else{
+                    issafe[c] = -1;
+                    visit[c] = false;
+                    return false;
+                }
+                visit[c] = false;
+            }
+            else if(issafe[c] == -1) return false;
+            else if(issafe[c] == 1) continue;
+        }
         return true;
-     }
+    }
+    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> terminal(n,0);
-        vector<int> vis(n,0);
-        for(int i = 0 ; i < n ; i++ ){
-            if(graph[i].empty())terminal[i] = 1;
-        }
-        vector<int> ans;
-        for(int i = 0 ; i< n; i++){
-            if(dfs(graph,terminal,vis,i)){
-                ans.push_back(i);
+        vector<bool> visit(n, false);
+        vector<int> issafe(n, 0);
+        vector<int> result;
+        
+        for(int i = 0; i < n; i++){
+            if(issafe[i] != 1){
+                visit[i] = true;
+                if(helper(graph, issafe, visit, i)){
+                    issafe[i] = 1;
+                }
+                else{
+                    issafe[i] = -1;
+                }
+                visit[i] = false;
             }
         }
-        return ans;
+        for(int i = 0; i < n; i++){
+            if(issafe[i] == 1){
+                result.push_back(i);
+            }
+        }
+        
+        return result;
     }
 };
