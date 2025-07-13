23b9608f801339b090c0a4b7351bd74244440321
Yagyeshz Vishnoi <154703590+yagyeshvishnoi@users.noreply.github.com>
2025-07-13 17:07:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.98%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int, int>>> adj(n);
-        for(auto& flight : flights) {
+        for (auto& flight : flights) {
             adj[flight[0]].push_back({flight[1], flight[2]});
         }
+        queue<tuple<int, int, int>> pq;
+        pq.push({0, 0, src});  
 
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-        pq.push({0, src, 0});
-        vector<vector<int>> dist(n, vector<int>(k + 2, INT_MAX));
-        
-        dist[src][0] = 0;
+        vector<int> dist(n, INT_MAX);  
+        dist[src] = 0;  
+
         while (!pq.empty()) {
-            auto [cost, node, stops] = pq.top();
+            auto [cost, stops, node] = pq.front();
             pq.pop();
-
-            if (node == dst) return cost;
-            if (stops > k) continue;
-
-            for (auto& [nextNode, price] : adj[node]) {
+            
+            if (stops > k) continue; 
+            
+            for (auto& [next, price] : adj[node]) {
                 int newCost = cost + price;
-                if (newCost < dist[nextNode][stops + 1]) {
-                    dist[nextNode][stops + 1] = newCost;
-                    pq.push({newCost, nextNode, stops + 1});
+
+                if (newCost < dist[next]) {
+                    dist[next] = newCost;
+                    pq.push({newCost, stops + 1, next});
                 }
             }
         }
 
-        return -1;
+        if(dist[dst] == INT_MAX){
+            return -1;
+        }
+        return dist[dst];
     }
 };
